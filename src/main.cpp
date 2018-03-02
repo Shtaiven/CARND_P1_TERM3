78768ecfc3082f1cd7d3ec47f72bfea90e0c8163
Steven Eisinger <etienneshorty@gmail.com>
2018-03-02 02:00:35 -0500

    Smooth follow

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -200,13 +201,19 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  // start in lane 1
+  int lane = 1;
+
+  // reference velocity to target
+  double ref_vel = 49.5; // mph
+
+  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
-    //auto sdata = string(data).substr(0, length);
-    //cout << sdata << endl;
+    // auto sdata = string(data).substr(0, length);
+    // cout << sdata << endl;
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -220,71 +227,158 @@ int main() {
           // j[1] is the data JSON object
 
           // Main car's localization Data
-            double car_x = j[1]["x"];
-            double car_y = j[1]["y"];
-            double car_s = j[1]["s"];
-            double car_d = j[1]["d"];
-            double car_yaw = j[1]["yaw"];
-            double car_speed = j[1]["speed"];
+          double car_x = j[1]["x"];
+          double car_y = j[1]["y"];
+          double car_s = j[1]["s"];
+          double car_d = j[1]["d"];
+          double car_yaw = j[1]["yaw"];
+          double car_speed = j[1]["speed"];
+          auto previous_path_x = j[1]["previous_path_x"];
+          auto previous_path_y = j[1]["previous_path_y"];
+          double end_path_s = j[1]["end_path_s"];
+          double end_path_d = j[1]["end_path_d"];
+          vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
-            // Previous path data given to the Planner
-            auto previous_path_x = j[1]["previous_path_x"];
-            auto previous_path_y = j[1]["previous_path_y"];
-            // Previous path's end s and d values
-            double end_path_s = j[1]["end_path_s"];
-            double end_path_d = j[1]["end_path_d"];
+          int prev_size = previous_path_x.size();
 
-            // Sensor Fusion Data, a list of all other cars on the same side of the road.
-            auto sensor_fusion = j[1]["sensor_fusion"];
+          if (prev_size > 0)
+          {
+            car_s = end_path_s;
+          }
 
-            json msgJson;
+          bool too_close = false;
+
+          for (int i = 0; i < sensor_fusion.size(); i++)
+          {
+            float d = sensor_fusion[i][6];
+            if (d < (2+4*lane+2) && d > (2+4*lane-2))
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx+vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+
+              check_car_s += (double) prev_size * 0.02 * check_speed;
+              // if the car is in front of us and less than 30 meters away
+              if ((check_car_s > car_s) && ((check_car_s - car_s) < 30))
+              {
+                ref_vel = 29.5; // mph
+                too_close = true;
+              }
+            }
+          }
+
+          if (too_close)
+          {
+            ref_vel -= .224;
+          }
+          else if (ref_vel < 49.5)
+          {
+            ref_vel += .224;
+          }
 
 /**************************** Start modifications ****************************/
+
+            vector<double> pts_x;
+            vector<double> pts_y;
+
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            if (prev_size < 2)
+            {
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              pts_x.push_back(prev_car_x);
+              pts_x.push_back(car_x);
+
+              pts_y.push_back(prev_car_y);
+              pts_y.push_back(car_y);
+            }
+            else
+            {
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+              pts_x.push_back(ref_x_prev);
+              pts_x.push_back(ref_x);
+
+              pts_y.push_back(ref_y_prev);
+              pts_y.push_back(ref_y);
+            }
+
+            vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            pts_x.push_back(next_wp0[0]);
+            pts_x.push_back(next_wp1[0]);
+            pts_x.push_back(next_wp2[0]);
+
+            pts_y.push_back(next_wp0[1]);
+            pts_y.push_back(next_wp1[1]);
+            pts_y.push_back(next_wp2[1]);
+
+            for (int i = 0; i < pts_x.size(); i++)
+            {
+              double shift_x = pts_x[i] - ref_x;
+              double shift_y = pts_y[i] - ref_y;
+
+              pts_x[i] = (shift_x * cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+              pts_y[i] = (shift_x * sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+            }
+
+            tk::spline s;
+
+            s.set_points(pts_x, pts_y);
 
             vector<double> next_x_vals;
             vector<double> next_y_vals;
 
-            double pos_x;
-            double pos_y;
-            double angle;
-            int path_size = previous_path_x.size();
-
-            for(int i = 0; i < path_size; i++)
+            for (int i = 0; i < previous_path_x.size(); i++)
             {
-                next_x_vals.push_back(previous_path_x[i]);
-                next_y_vals.push_back(previous_path_y[i]);
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
             }
 
-            if(path_size == 0)
+            double target_x = 30.0;
+            double target_y = s(target_x);
+            double target_dist = sqrt(target_x*target_x+target_y*target_y);
+
+            double x_add_on = 0;
+
+            for (int i = 1; i <= 50-previous_path_x.size(); i++)
             {
-                pos_x = car_x;
-                pos_y = car_y;
-                angle = deg2rad(car_yaw);
-            }
-            else
-            {
-                pos_x = previous_path_x[path_size-1];
-                pos_y = previous_path_y[path_size-1];
+              double N = target_dist/(0.02*ref_vel/2.24);
+              double x_point = x_add_on+target_x/N;
+              double y_point = s(x_point);
 
-                double pos_x2 = previous_path_x[path_size-2];
-                double pos_y2 = previous_path_y[path_size-2];
-                angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
-            }
+              x_add_on = x_point;
 
-            double dist_inc = 0.5;
-            for(int i = 0; i < 50-path_size; i++)
-            {
-                next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
-                next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
-                pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
-                pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+              double x_ref = x_point;
+              double y_ref = y_point;
+
+              x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+              y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+              x_point += ref_x;
+              y_point += ref_y;
+
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
             }
 
+// /***************************** End modifications *****************************/
 
+            json msgJson;
             msgJson["next_x"] = next_x_vals;
             msgJson["next_y"] = next_y_vals;
-
-/***************************** End modifications *****************************/
 
             auto msg = "42[\"control\","+ msgJson.dump()+"]";
 

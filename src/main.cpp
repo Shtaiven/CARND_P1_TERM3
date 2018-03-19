532d85b08e5f7a926048706b3e4ccb84298ec810
Steven Eisinger <etienneshorty@gmail.com>
2018-03-19 00:58:09 -0400

    Almost there, need to account for size of cars

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,11 @@ struct trajectory_t {
   int final_lane;
 };
 
+struct point_t {
+  double x;
+  double y;
+};
+
 // Vehicle data POD
 struct vehicle_t {
   double id;
@@ -216,6 +221,66 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
   return {x,y};
 
+}
+
+// Below are 3 functions used for line segment intersection calculations taken from https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+// Given three colinear points p, q, r, the function checks if
+// point q lies on line segment 'pr'
+bool onSegment(point_t p, point_t q, point_t r)
+{
+    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
+        q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
+       return true;
+
+    return false;
+}
+
+// To find orientation of ordered triplet (p, q, r).
+// The function returns following values
+// 0 --> p, q and r are colinear
+// 1 --> Clockwise
+// 2 --> Counterclockwise
+int orientation(point_t p, point_t q, point_t r)
+{
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/
+    // for details of below formula.
+    int val = (q.y - p.y) * (r.x - q.x) -
+              (q.x - p.x) * (r.y - q.y);
+
+    if (val == 0) return 0;  // colinear
+
+    return (val > 0)? 1: 2; // clock or counterclock wise
+}
+
+// The main function that returns true if line segment 'p1q1'
+// and 'p2q2' intersect.
+bool doIntersect(point_t p1, point_t q1, point_t p2, point_t q2)
+{
+    // Find the four orientations needed for general and
+    // special cases
+    int o1 = orientation(p1, q1, p2);
+    int o2 = orientation(p1, q1, q2);
+    int o3 = orientation(p2, q2, p1);
+    int o4 = orientation(p2, q2, q1);
+
+    // General case
+    if (o1 != o2 && o3 != o4)
+        return true;
+
+    // Special Cases
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+
+    // p1, q1 and q2 are colinear and q2 lies on segment p1q1
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+
+     // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+    return false; // Doesn't fall in any of the above cases
 }
 
 /*****************************************************************************
@@ -523,17 +588,40 @@ double calculate_cost(trajectory_t trajectory, vector<trajectory_t> observed_tra
     return cost;
   }
 
-  bool collision = false;
-
-  for (int i = 0; i < observed_trajectories.size(); ++i)
+  // Check for trajectory collisions here
+  if (trajectory.x_vec.size() > 1)
   {
-    // TODO: check for trajectory collisions here
-  }
+    // linear estimation of cars trajectory (for speed of computation)
+    point_t p1;
+    p1.x = trajectory.x_vec[0];
+    p1.y = trajectory.y_vec[0];
 
-  // if there will be a collision with an observed vehicle
-  if (collision)
-  {
-    return cost;
+    point_t q1;
+    q1.x = trajectory.x_vec[trajectory.x_vec.size()-1];
+    q1.y = trajectory.y_vec[trajectory.y_vec.size()-1];
+
+    // TODO: account for size of cars
+    for (int i = 0; i < observed_trajectories.size(); ++i)
+    {
+
+      for (int j = 0; j < observed_trajectories[i].x_vec.size()-1; ++j)
+      {
+        point_t p2;
+        p2.x = observed_trajectories[i].x_vec[j];
+        p2.y = observed_trajectories[i].y_vec[j];
+
+        point_t q2;
+        q2.x = observed_trajectories[i].x_vec[j+1];
+        q2.y = observed_trajectories[i].y_vec[j+1];
+
+        // if there will be a collision with an observed vehicle, return the cost
+        if (doIntersect(p1, q1, p2, q2))
+        {
+          printf("traj %d, iter %d (%lf, %lf) (%lf, %lf) intersects (%lf, %lf) (%lf, %lf)\n", i, j, p1.x ,p1.y, p2.x, p2.y, q1.x, q1.y, q2.x, q2.y);
+          return cost;
+        }
+      }
+    }
   }
 
   // Rank trajectory based on distance from speed limit
